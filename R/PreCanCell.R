7da222d0d3ce465e701fae3805bd7f5b0fa6efcd
WangX-Lab <54932820+WangX-Lab@users.noreply.github.com>
2022-11-28 16:08:39 +0800

    Delete R directory

--- R/PreCanCell.R
+++
@@ -1,9 +0,0 @@
-#' Prediction of cancer and non-cancer cells from single-cell transcriptomes
-#'
-#' A single cell was classified as malignant or non-malignant based on its gene expression profile by the k-NN (k = 5) classifier.
-#'
-#'
-#' @docType package
-#' @name PreCanCell-package
-#' @aliases PreCanCell
-NULL
