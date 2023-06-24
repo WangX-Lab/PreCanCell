a4f5dc0097afddcefd3fa93e9a8ca4c1ecb85d0c
WangX-Lab <54932820+WangX-Lab@users.noreply.github.com>
2023-06-24 20:18:59 +0800

    Update Custom_preprocessing.R

--- R/Custom_preprocessing.R
+++ R/Custom_preprocessing.R
@@ -15,8 +15,9 @@
 
 Custom_preprocessing <- function(counts, annotation, project = "PanCanCell", min.cells = 400, min.features = 0,
                                  normalization.method = "LogNormalize", scale.factor = 10000,
-                                 verbose = TRUE) {
-  data("feature", package = "PreCanCell") # loading feature datasets
+                                 verbose = TRUE){
+  library(Seurat)
+  data("feature",package = "PreCanCell") # loading feature datasets
   data <- CreateSeuratObject(counts = counts, project = project, min.cells = min.cells, min.features = min.features)
   data <- NormalizeData(object = data, normalization.method = normalization.method, scale.factor = scale.factor, verbose = verbose)
   data <- GetAssayData(data, slot="data")
@@ -25,6 +26,6 @@ Custom_preprocessing <- function(counts, annotation, project = "PanCanCell", min
   data <- apply(t(as.data.frame(data)), 2, function(x) {
     (x - min(x)) / (max(x) - min(x))
   })
-  data <- as.data.frame(CancerLabel = annotation[,2], data)
+  data <- as.data.frame(CancerLabel=annotation[,2],data)
   return(data)
 }
